2af3488294db3f903d2148108f2bc57df2cfc282
dirtynight <r.dara@gmx.de>
2018-05-22 11:08:52 +0200

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,14 @@
+<?PHP
+// Kalenderapp 
+
+define ( 'MYSQL_HOST','localhost' );
+define ( 'MYSQL_USER'. 'root');
+define ( 'MYSQL_KENNWORT','' );
+define ('MYSQL_DATENBANK', 'kalenderapp');
+
+
+$dblink = @mysql_connect(
+
+
+
+?>
