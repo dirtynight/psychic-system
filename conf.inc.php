3a12f69c0a52118541720cbd46637e5f52243a9f
dirtynight <r.dara@gmx.de>
2018-05-22 21:16:56 +0200

    Update conf.inc.php

--- conf.inc.php
+++ conf.inc.php
@@ -9,9 +9,9 @@ define ('DB_HOST', 'localhost');
 define ('DB_USER', 'root');
 define ('DB_PASS', '');
 
-$db_link = @mysql_connect (MYSQL_HOST,
-                           MYSQL_BENUTZER,
-                           MYSQL_KENNWORT);
+$db_link = @mysql_connect (DB_HOST,
+                           DB_USER,
+                           DB_PASS);
 
 if(!$db_link){
     die("Sach mal einen Satz mit X");
